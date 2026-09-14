d28e807528f7a058ddc2ef570a147a7723cd0267
arash-jj <arshjfry66@gmail.com>
2026-09-14 12:09:06 +0330

    feat: add more options to config schema and fix `ignore` options validator

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -1,6 +1,8 @@
 import z from "zod";
 
 export const ConfigSchema = z.object({
-  ignore: z.array(z.string()).nonempty().optional(),
+  ignore: z.array(z.string()).default([]),
   customPatterns: z.string().min(1).optional(),
+  failOn: z.enum(["critical", "high", "medium", "low"]).default("medium"),
+  include: z.array(z.string()).optional(),
 });
